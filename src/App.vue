444c5791b9ca126baca948900996de542fddec1b
包子君 <9478383+bao-zijun@user.noreply.gitee.com>
2024-07-18 17:32:01 +0800

    线性雾

--- src/App.vue
+++ src/App.vue
@@ -64,71 +64,23 @@ window.addEventListener("resize", () => {
 let params = {};
 //创建gui
 const gui = new GUI();
-//创建纹理添加器
-let textureLoader = new THREE.TextureLoader();
-//加载纹理
-let texture = textureLoader.load(
-  "./texture/watercover/CityNewYork002_COL_VAR1_1K.png"
-);
+//创建长方体
+const boxGeometry = new THREE.BoxGeometry(1,1,100)
+//创建金属材质
+const boxMateral = new THREE.MeshBasicMaterial({
+  color:0x00ff00
+})
+const box = new THREE.Mesh(boxGeometry,boxMateral)
+scene.add(box)
+//创建场景雾
+// scene.fog = new THREE.Fog(0x999999,0.1,50)
+//创建场景指数
+scene.fog = new THREE.FogExp2(0x999999)
+//背景颜色添加
+scene.background = new THREE.Color(0x999999)
 
-//添加纹理的颜色空间
-texture.colorSpace = THREE.SRGBColorSpace;
-// texture.colorSpace = THREE.LinearSRGBColorSpace;
-// texture.colorSpace = THREE.NoColorSpace;
 
-//加载ao贴图(让边缘更亮一些)
-let aoMap = textureLoader.load("./texture/watercover/CityNewYork002_AO_1K.jpg");
 
-//设置透明度贴图
-let alphaMap = textureLoader.load("./texture/door/height.jpg");
-
-//设置光照贴图
-let lightMap = textureLoader.load("./texture/colors.png");
-
-// 高光贴图
-let specularMap = textureLoader.load(
-  "./texture/watercover/CityNewYork002_GLOSS_1K.jpg"
-);
-
-//rgbeLoader加载
-let rgbeLoader = new RGBELoader();
-rgbeLoader.load("./texture/Alex_Hart-Nature_Lab_Bones_2k.hdr", (envMap) => {
-  //设置球形贴图
-  envMap.mapping = THREE.EquirectangularReflectionMapping;
-  //设置环境贴图
-  scene.background = envMap;
-  //场景设置环境贴图
-  scene.environment = envMap;
-  //给平面的材质设置环境贴图
-  planeMaterial.environment = envMap;
-});
-
-let planeGeomoetry = new THREE.PlaneGeometry(1, 1);
-let planeMaterial = new THREE.MeshBasicMaterial({
-  // color: 0xffff00,
-  map: texture,
-  transparent: true, //设置透明度
-  aoMap: aoMap,
-  aoMapIntensity: 1, //设置透明度额强度，默认是1
-  // alphaMap: alphaMap, //透明度贴图
-  // lightMap:lightMap,//光照贴图
-  reflectivity: 0.5,
-  specularMap: specularMap, //高光贴图（比较亮的地方）
-});
-let plane = new THREE.Mesh(planeGeomoetry, planeMaterial);
-scene.add(plane);
-// gui.add(planeMaterial,'transparent','透明')
-gui.add(planeMaterial, "aoMapIntensity").min(0).max(1).name("ao贴图");
-
-//更改颜色空间
-gui
-  .add(texture, "colocSpace", {
-    sRGB: THREE.SRGBColorSpace,
-    Linear: THREE.LinearSRGBColorSpace,
-  })
-  .onChange(() => {
-    texture.needsUpdate = true;
-  });
 </script>
 
 <template>
